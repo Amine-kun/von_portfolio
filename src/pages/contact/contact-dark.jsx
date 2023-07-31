676804c5da46d51073330d5944a44cf2fc9e84dd
unknown <aminehlab@gmail.com>
2023-07-31 18:53:18 +0100

    first commit

---
+++ src/pages/contact/contact-dark.jsx
@@ -0,0 +1,54 @@
+/* eslint-disable @next/next/no-sync-scripts */
+import React from "react";
+import Navbar from "../../components/Navbar/navbar";
+import Footer from "../../components/Footer/footer";
+import DarkTheme from "../../layouts/Dark";
+import ContactHeader from "../../components/Contact-header/contact-header";
+import ContactForm from "../../components/Contact-form/contact-form";
+
+const Contact = () => {
+  const navbarRef = React.useRef(null);
+  const logoRef = React.useRef(null);
+
+  React.useEffect(() => {
+    document.querySelector("body").classList.add("contact-page");
+
+    var navbar = navbarRef.current,
+      logo = logoRef.current;
+    if (window.pageYOffset > 300) {
+      navbar.classList.add("nav-scroll");
+    } else {
+      navbar.classList.remove("nav-scroll");
+    }
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        navbar.classList.add("nav-scroll");
+      } else {
+        navbar.classList.remove("nav-scroll");
+      }
+    });
+    return () => {
+      document.querySelector("body").classList.remove("contact-page");
+    };
+  }, [navbarRef]);
+
+  return (
+    <DarkTheme>
+      <Navbar nr={navbarRef} lr={logoRef} />
+      <ContactHeader />
+      <div className="main-content">
+        <ContactForm />
+        <div className="map" id="ieatmaps">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d53940.6513922368!2d-6.373229!3d32.33090430000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sma!4v1684676575135!5m2!1sen!2sma"
+            loading="lazy"
+          ></iframe>
+        </div>
+
+        <Footer hideBGCOLOR />
+      </div>
+    </DarkTheme>
+  );
+};
+
+export default Contact;
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "./homepage/home5-dark";
+
+const Home = () => {
+  return (
+    <>
+      <Main />
+    </>
+  );
+};
+
+export default Home;
